3c6cf7201339b8f1ddcc64be4252a55a5c9feab8
André Rodrigues <andre.rodrigues0018@gmail.com>
2024-05-28 22:44:41 -0300

    feat: implement a simple query in gemini

--- api/src/server.js
+++ api/src/server.js
@@ -8,8 +8,36 @@ const port = 3000;
 const produtos = [123]; // Array para armazenar produtos
 
 // Rota GET para obter todos os produtos
-app.get('/produtos', (req, res) => {
-    res.json( process.env.API_KEY);
+app.get('/gemini/test', (req, res) => {
+    const payload = {
+        "contents": [
+            {
+                "parts": [
+                    { "text": "De maneira breve, Qual foi o ultimo rei de roma" }
+                ]
+            }
+        ]
+    };
+    const API_KEY = process.env.API_KEY
+
+    fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + API_KEY, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data.candidates){
+                res.json(data.candidates[0].content.parts[0].text);
+            }else{
+                res.status(400)
+            }
+        });
+
+
+
 });
 
 // Rota POST para criar um novo produto
